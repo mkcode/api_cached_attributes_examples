ff3452b89f0cea37667b892abd038c79568c6442
Chris Ewald <chrisewald@gmail.com>
2016-03-14 23:37:37 -0400

    mixin GithubUserAttributes to the GithubUser model

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,2 +1,5 @@
 class GithubUser < ActiveRecord::Base
+  api_cached_attributes :github_user, scope: :github_login,
+    attributes_map: { id: :github_id, updated_at: :github_updated_at,
+                      created_at: :github_created_at }
 end
