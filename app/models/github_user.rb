c27bbb53e1ed7d706266c73a2f0ff92ac2b1d513
Chris Ewald <chrisewald@gmail.com>
2016-03-14 23:46:04 -0400

    add presence and uniqueness validator to gh_login

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,4 +1,6 @@
 class GithubUser < ActiveRecord::Base
+  validates :github_login, presence: true, uniqueness: true
+
   api_cached_attributes :github_user, scope: :github_login,
     attributes_map: { id: :github_id, updated_at: :github_updated_at,
                       created_at: :github_created_at }
